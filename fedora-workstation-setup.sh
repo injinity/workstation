9c13dc145a079fa36b915c78fde2d84b8ade443b
Dastro <57630037+Dastroo@users.noreply.github.com>
2024-02-21 18:09:47 +0100

    Update fedora-workstation-setup.sh

--- fedora-workstation-setup.sh
+++ fedora-workstation-setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Hiding the default browser (Firefox)
+# https://docs.fedoraproject.org/en-US/fedora-silverblue/tips-and-tricks/#_hiding_the_default_browser_firefox
+mkdir -p /usr/local/share/applications/
+cp /usr/share/applications/firefox.desktop /usr/local/share/applications/
+sed -i "2a\\NotShowIn=GNOME;KDE" /usr/local/share/applications/firefox.desktop
+update-desktop-database /usr/local/share/applications/
+
 # Set mouse scroll directions to natural
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
