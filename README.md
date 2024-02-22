2840c37bebee681a31493c6b7206c6a3c3b005ca
Dastroo <admin@injinity.org>
2024-02-22 18:11:54 +0100

    feat: add lots of new programs

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # Fedora Workstation Setup
 Scripts and instructions for setting up Fedora Silverblue
 
-# Prerequisites
+## Prerequisites
+
 - Fresh Fedora Silverblue installation  
 [official guide](https://docs.fedoraproject.org/en-US/fedora/latest/preparing-boot-media/#_fedora_media_writer)  
 ***(make sure to choose `Fedora Silverblue` in the media writer)***
-- Github ssh-key setup  
+- GitHub ssh-key setup  
 [instructions](https://github.com/injinity/instructions/blob/main/gtihub_ssh_auth/README.md)
 
-# How to use
+## How to use
+
 - Clone this repository  
 `git clone git@github.com:injinity/fedora-workstation-setup.git`
 - Change directory to the cloned repository  
--- fedora-workstation-setup.sh
+++ fedora-workstation-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install Brave browser and set it as default
+flatpak install -y com.brave.Browser
+xdg-settings set default-web-browser com.brave.Browser.desktop
+
 # Hiding the default browser (Firefox)
 # https://docs.fedoraproject.org/en-US/fedora-silverblue/tips-and-tricks/#_hiding_the_default_browser_firefox
 mkdir -p /usr/local/share/applications/
@@ -9,3 +13,24 @@ update-desktop-database /usr/local/share/applications/
 
 # Set mouse scroll directions to natural
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
+
+# Install Flatseal
+flatpak install -y flathub com.github.tchx84.Flatseal
+
+# Install Intelij IDEA CE
+flatpak install -y com.jetbrains.IntelliJ-IDEA-Community
+
+# Install Steam
+flatpak install -y com.valvesoftware.Steam
+
+# Install Spotify
+flatpak install -y com.spotify.Client
+
+# Install Signal
+flatpak install -y org.signal.Signal
+
+# Install Lutris
+toolbox create -y
+toolbox run sudo dnf update -y && sudo dnf upgrade -y
+toolbox run sudo dnf install -y wine python3-cairo gnome-terminal vulkan-tools pciutils fluidsynth
+toolbox run sudo dnf install -y lutris
