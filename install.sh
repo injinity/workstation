8b288b2e24006244ae2f642e8fa9a47cb7e24dd4
Dawid <admin@injinity.org>
2025-03-26 12:38:58 +0100

    feat(injinity): add injinity script

--- install.sh
+++ install.sh
@@ -2,14 +2,15 @@
 
 sudo chmod +x scripts/*.sh scripts/subscripts/*.sh
 
-./scripts/browser.sh
-./scripts/general.sh
-./scripts/programming.sh
-./scripts/gaming.sh
+./scripts/browser.sh "$@"
+./scripts/general.sh "$@"
+./scripts/programming.sh "$@"
+./scripts/gaming.sh "$@"
+./scripts/injinity.sh "$@"
 
 # Loop through all arguments
 for arg in "$@"; do
-    if [[ "$arg" == "--no-programming" ]]; then
+    if [[ "$arg" == "--nvidia" ]]; then
         echo "Option --nvidia detected, installing nvidia gpu drivers."
         ./scripts/nvidia_gpu_driers.sh
     fi
--- scripts/injinity.sh
+++ scripts/injinity.sh
@@ -10,13 +10,30 @@ done
 
 toolbox run sudo dnf install kubectl
 
+# Add autocompletion for kubectl CLI
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#enable-shell-autocompletion
+toolbox run echo 'source <(kubectl completion bash)' >>~/.bashrc
+
 # Install flux CLI
 # https://fluxcd.io/flux/cmd/#install-using-bash
 toolbox run sudo curl -s https://fluxcd.io/install.sh | sudo FLUX_VERSION=2.0.0 bash
 
-# Add autocompletion to the flux command
+# Add autocompletion for flux CLI
 # https://fluxcd.io/flux/cmd/flux_completion_bash/#examples
 # ~/.bashrc or ~/.profile
 toolbox run echo "command -v flux >/dev/null && . <(flux completion bash)" | sudo tee -a ~/.bashrc
 
+# Set a local domain "vps1.contabo-0" and point it to the ip of the VPS
+echo "62.169.17.115\tvps1.contabo-0" | sudo tee -a /etc/hosts
+
+# Ssh tunnel to the master-node-0
+ssh -i $HOME/.ssh/dev.vps1.contabo-0 -fNL 6443:127.0.0.1:6443 dev@vps1.contabo-0
+
+# Download the KUBECONFIG from server
+toolbox run scp -i $HOME/.ssh/dev.vps1.contabo-0 dev@vps1.contabo-0:/etc/rancher/rke2/rke2.yaml .kube/config
+
+mkdir ~/Nodes
+touch ~/Nodes/vps1.contabo-0.sh
+echo -e '#!/bin/bash\n\nssh -i $HOME/.ssh/dev.vps1.contabo-0 dev@vps1.contabo-0' > ~/Nodes/dev.vps1.contabo-0.sh
+sudo chmod +x ~/Nodes/dev.vps1.contabo-0.sh
 
